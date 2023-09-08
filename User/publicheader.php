7341e13ea185f7e99e0ef43cf4fad43ef8cfb2f8
Akash <akashmdy123@gmail.com>
2023-09-08 17:05:17 +0530

    header updated

--- User/publicheader.php
+++ User/publicheader.php
@@ -1,215 +1,221 @@
 <header class="header-v1">
-		<!-- Header desktop -->
-		<div class="container-menu-desktop trans-03">
+	<!-- Header desktop -->
+	<div class="container-menu-desktop trans-03">
 
 
-			<div class="top-bar">
-				<div class="content-topbar flex-sb-m h-full container">
-					<div class="left-top-bar">
-						Some text here
-					</div>
-
-					<div class="right-top-bar flex-w h-full">
-						<a href="#" class="flex-c-m trans-04 p-lr-25">
-							Help & FAQs
-						</a>
-
-						<a href="#" class="flex-c-m trans-04 p-lr-25">
-							My Account
-						</a>
-
-						
-					</div>
+		<div class="top-bar">
+			<div class="content-topbar flex-sb-m h-full container">
+				<div class="left-top-bar">
+					Some text here
 				</div>
-			</div>
-			
-			<div class="wrap-menu-desktop">
-				<nav class="limiter-menu-desktop p-l-45">
-					
-					<!-- Logo desktop -->		
-					<a href="#" class="logo">
-						<img src="images/icons/riro_logo.png" alt="IMG-LOGO">
+
+				<div class="right-top-bar flex-w h-full">
+					<a href="#" class="flex-c-m trans-04 p-lr-25">
+						Help & FAQs
 					</a>
 
-					<!-- Menu desktop -->
-					<div class="menu-desktop">
-						<ul class="main-menu">
-							<li class="active-menu">
-								<a href="index.php">Home</a>
-								
-							</li>
+					<a href="#" class="flex-c-m trans-04 p-lr-25">
+						My Account
+					</a>
 
-							<li>
-								<a href="product.php">Shop</a>
-							</li>
 
-							<li>
-								<a href="gallery.php">Gallery</a>
-							</li>
-
-							<li>
-								<a href="about.php">About</a>
-							</li>
-
-							<li>
-								<a href="contact.php">Contact</a>
-							</li>
-						</ul>
-					</div>	
-
-					<!-- Icon header -->
-					<div class="wrap-icon-header flex-w flex-r-m h-full">
-						
-							
-						<div class="flex-c-m h-full p-l-18 p-r-25 bor5">
-							<div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart" data-notify="2">
-								<i class="zmdi zmdi-shopping-cart"></i>
-							</div>
-						</div>
-							
-						
-					</div>
-				</nav>
-			</div>	
-		</div>
-
-		<!-- Header Mobile -->
-		<div class="wrap-header-mobile ">
-			<!-- Logo moblie -->		
-			<div class="logo-mobile">
-				<a href="index.php"><img src="images/icons/favicon.png" alt="IMG-LOGO" ></a>
-			</div>
-
-			<!-- Icon header -->
-			<div class="wrap-icon-header flex-w flex-r-m h-full m-r-15">
-				
-
-				<div class="flex-c-m h-full p-lr-10 bor5">
-					<div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart " data-notify="2">
-						<i class="zmdi zmdi-shopping-cart"></i>
-					</div>
 				</div>
-			</div>
-
-			<!-- Button show menu -->
-			<div class="btn-show-menu-mobile hamburger hamburger--squeeze text-white">
-				<span class="hamburger-box text-white">
-					<span class="hamburger-inner"></span>
-				</span>
 			</div>
 		</div>
 
+		<div class="wrap-menu-desktop">
+			<nav class="limiter-menu-desktop p-l-45">
 
-		<!-- Menu Mobile -->
-		<div class="menu-mobile">
-			<ul class="main-menu-m">
-				<li>
-					<a href="index.php">Home</a>
-					
-				</li>
+				<!-- Logo desktop -->
+				<a href="#" class="logo">
+					<img src="images/icons/riro_logo.png" alt="IMG-LOGO">
+				</a>
 
-				<li>
-					<a href="product.php">Shop</a>
-				</li>
+				<!-- Menu desktop -->
+				<?php
+				// Get the current page's filename
+				$current_page = basename($_SERVER['PHP_SELF']);
+				?>
 
-				<li>
-					<a href="gallery.php">Gallery</a>
-				</li>
-
-				<li>
-					<a href="about.php">About</a>
-				</li>
-
-				<li>
-					<a href="contact.php">Contact</a>
-				</li>
-			</ul>
-		</div>
-
-		
-	</header>
-
-	<!-- Cart -->
-	<div class="wrap-header-cart js-panel-cart">
-		<div class="s-full js-hide-cart"></div>
-
-		<div class="header-cart flex-col-l p-l-65 p-r-25">
-			<div class="header-cart-title flex-w flex-sb-m p-b-8">
-				<span class="mtext-103 cl2">
-					Your Cart
-				</span>
-
-				<div class="fs-35 lh-10 cl2 p-lr-5 pointer hov-cl1 trans-04 js-hide-cart">
-					<i class="zmdi zmdi-close"></i>
+				<!-- Menu desktop -->
+				<div class="menu-desktop">
+					<ul class="main-menu">
+						<li id="menu-home" <?php if ($current_page === 'index.php')
+							echo 'class="active-menu"'; ?>><a
+								href="index.php">Home</a></li>
+						<li id="menu-shop" <?php if ($current_page === 'product.php')
+							echo 'class="active-menu"'; ?>><a
+								href="product.php">Shop</a></li>
+						<li id="menu-gallery" <?php if ($current_page === 'gallery.php')
+							echo 'class="active-menu"'; ?>><a
+								href="gallery.php">Gallery</a></li>
+						<li id="menu-about" <?php if ($current_page === 'about.php')
+							echo 'class="active-menu"'; ?>><a
+								href="about.php">About</a></li>
+						<li id="menu-contact" <?php if ($current_page === 'contact.php')
+							echo 'class="active-menu"'; ?>><a
+								href="contact.php">Contact</a></li>
+					</ul>
 				</div>
-			</div>
-			
-			<!-- <div class="header-cart-content flex-w js-pscroll">
-				<ul class="header-cart-wrapitem w-full">
-					<li class="header-cart-item flex-w flex-t m-b-12">
-						<div class="header-cart-item-img">
-							<img src="images/item-cart-01.jpg" alt="IMG">
+
+
+				<!-- Icon header -->
+				<div class="wrap-icon-header flex-w flex-r-m h-full">
+
+
+					<div class="flex-c-m h-full p-l-18 p-r-25 bor5">
+						<div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart"
+							data-notify="2">
+							<i class="zmdi zmdi-shopping-cart"></i>
 						</div>
-
-						<div class="header-cart-item-txt p-t-8">
-							<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
-								White Shirt Pleat
-							</a>
-
-							<span class="header-cart-item-info">
-								1 x $19.00
-							</span>
-						</div>
-					</li>
-
-					<li class="header-cart-item flex-w flex-t m-b-12">
-						<div class="header-cart-item-img">
-							<img src="images/item-cart-02.jpg" alt="IMG">
-						</div>
-
-						<div class="header-cart-item-txt p-t-8">
-							<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
-								Converse All Star
-							</a>
-
-							<span class="header-cart-item-info">
-								1 x $39.00
-							</span>
-						</div>
-					</li>
-
-					<li class="header-cart-item flex-w flex-t m-b-12">
-						<div class="header-cart-item-img">
-							<img src="images/item-cart-03.jpg" alt="IMG">
-						</div>
-
-						<div class="header-cart-item-txt p-t-8">
-							<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
-								Nixon Porter Leather
-							</a>
-
-							<span class="header-cart-item-info">
-								1 x $17.00
-							</span>
-						</div>
-					</li>
-				</ul>
-				
-				<div class="w-full">
-					<div class="header-cart-total w-full p-tb-40">
-						Total: $75.00
 					</div>
 
-					<div class="header-cart-buttons flex-w w-full">
-						<a href="shoping-cart.html" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10">
-							View Cart
-						</a>
 
-						<a href="shoping-cart.html" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-b-10">
-							Check Out
-						</a>
-					</div>
 				</div>
-			</div> -->
+			</nav>
 		</div>
 	</div>
+
+	<!-- Header Mobile -->
+	<div class="wrap-header-mobile ">
+		<!-- Logo moblie -->
+		<div class="logo-mobile">
+			<a href="index.php"><img src="images/icons/favicon.png" alt="IMG-LOGO"></a>
+		</div>
+
+		<!-- Icon header -->
+		<div class="wrap-icon-header flex-w flex-r-m h-full m-r-15">
+
+
+			<div class="flex-c-m h-full p-lr-10 bor5">
+				<div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart "
+					data-notify="2">
+					<i class="zmdi zmdi-shopping-cart"></i>
+				</div>
+			</div>
+		</div>
+
+		<!-- Button show menu -->
+		<div class="btn-show-menu-mobile hamburger hamburger--squeeze text-white">
+			<span class="hamburger-box text-white">
+				<span class="hamburger-inner"></span>
+			</span>
+		</div>
+	</div>
+
+
+	<!-- Menu Mobile -->
+	<div class="menu-mobile">
+		<ul class="main-menu-m">
+			<li>
+				<a href="index.php">Home</a>
+
+			</li>
+
+			<li>
+				<a href="product.php">Shop</a>
+			</li>
+
+			<li>
+				<a href="gallery.php">Gallery</a>
+			</li>
+
+			<li>
+				<a href="about.php">About</a>
+			</li>
+
+			<li>
+				<a href="contact.php">Contact</a>
+			</li>
+		</ul>
+	</div>
+
+
+</header>
+
+<!-- Cart -->
+<div class="wrap-header-cart js-panel-cart">
+	<div class="s-full js-hide-cart"></div>
+
+	<div class="header-cart flex-col-l p-l-65 p-r-25">
+		<div class="header-cart-title flex-w flex-sb-m p-b-8">
+			<span class="mtext-103 cl2">
+				Your Cart
+			</span>
+
+			<div class="fs-35 lh-10 cl2 p-lr-5 pointer hov-cl1 trans-04 js-hide-cart">
+				<i class="zmdi zmdi-close"></i>
+			</div>
+		</div>
+
+		<div class="header-cart-content flex-w js-pscroll">
+			<ul class="header-cart-wrapitem w-full">
+				<li class="header-cart-item flex-w flex-t m-b-12">
+					<div class="header-cart-item-img">
+						<img src="images/item-cart-01.jpg" alt="IMG">
+					</div>
+
+					<div class="header-cart-item-txt p-t-8">
+						<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
+							White Shirt Pleat
+						</a>
+
+						<span class="header-cart-item-info">
+							1 x $19.00
+						</span>
+					</div>
+				</li>
+
+				<li class="header-cart-item flex-w flex-t m-b-12">
+					<div class="header-cart-item-img">
+						<img src="images/item-cart-02.jpg" alt="IMG">
+					</div>
+
+					<div class="header-cart-item-txt p-t-8">
+						<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
+							Converse All Star
+						</a>
+
+						<span class="header-cart-item-info">
+							1 x $39.00
+						</span>
+					</div>
+				</li>
+
+				<li class="header-cart-item flex-w flex-t m-b-12">
+					<div class="header-cart-item-img">
+						<img src="images/item-cart-03.jpg" alt="IMG">
+					</div>
+
+					<div class="header-cart-item-txt p-t-8">
+						<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
+							Nixon Porter Leather
+						</a>
+
+						<span class="header-cart-item-info">
+							1 x $17.00
+						</span>
+					</div>
+				</li>
+			</ul>
+
+			<div class="w-full">
+				<div class="header-cart-total w-full p-tb-40">
+					Total: $75.00
+				</div>
+
+				<div class="header-cart-buttons flex-w w-full">
+					<a href="shoping-cart.html"
+						class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10">
+						View Cart
+					</a>
+
+					<a href="shoping-cart.html"
+						class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-b-10">
+						Check Out
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
